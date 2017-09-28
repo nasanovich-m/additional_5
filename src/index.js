e00b8e3536630a34e5d91abaf4462872f20239ea
nasanovich-m <31859854+nasanovich-m@users.noreply.github.com>
2017-09-28 13:10:25 +0300

    Implement functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    var sequence = [];
+
+    var openBrackets = bracketsConfig.map(function (config) {
+        return config[0];
+    });
+
+    var closeBrackets = bracketsConfig.map(function (config) {
+        return config[1];
+    });
+
+    for (var i = 0, openIndex, closeIndex; i < str.length; i++) {
+        var char = str[i];
+
+        openIndex = openBrackets.indexOf(char);
+        closeIndex = closeBrackets.indexOf(char);
+
+        if (openIndex === closeIndex) {
+            if (closeIndex === sequence[sequence.length - 1]) {
+                openIndex = -1;
+            }
+        }
+
+        if (openIndex !== -1) {
+            sequence.push(openIndex);
+        } else if (closeIndex !== -1) {
+            var lastIndex = sequence.pop();
+
+            if (closeIndex !== lastIndex) {
+                return false;
+            }
+        }
+    }
+
+    return sequence.length === 0;
+};
